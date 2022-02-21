8b36a01073d3f5d9de79d47e7b0bb7808ab243f3
carlitos_madrigal <madrigalgallego.jc@gmail.com>
2022-02-21 20:27:41 +0100

    Funcion button pdte de hacer eventListener

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-const newDiv = document.createElement("aside")
 const button = document.createElement('button');
 button.type = 'button';
 button.innerText = 'Volver a la Página principal';
 document.body.appendChild(button)
+
+
