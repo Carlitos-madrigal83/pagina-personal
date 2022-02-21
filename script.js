6e240f0afeec3fedf84ee0f6bc7f66673a247a00
carlitos_madrigal <madrigalgallego.jc@gmail.com>
2022-02-21 21:01:49 +0100

    Pdte de añadir evento

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const button = document.createElement('button');
+/*const button = document.createElement('button');
 button.type = 'button';
 button.innerText = 'Volver a la Página principal';
-document.body.appendChild(button)
+document.body.appendChild(button)*/
 
 
